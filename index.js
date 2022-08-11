44c53b8bc0619269fffd992fe2a53b7253e2e174
Jeff <odhiambo47@gmail.com>
2022-08-12 00:48:50 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const name = "Jeff"
+
+function introduction(name) {
+    return `Hi, my name is ${name}.`
+}
+
+const language = "javaScript"
+
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
